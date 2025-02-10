b6ea58fcfca9f271e69a3b6e7c1c4883f6ad91bc
Dorian Grasset <dorian.grasset.contact@gmail.com>
2025-02-10 21:54:21 +0100

    feat(ui): setup Tailwind CSS and ShadCN
    - Installed Tailwind CSS and initialized configuration files.
    - Installed ShadCN components and set up base styles.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 function App() {
-  return <div></div>;
+  return <div>
+    <div className='bg-green-500'>Tailwindcss should work ! :)</div>
+  </div>;
 }
 
 export default App;
