86bd8bcb513c686f3d8fe99d710aee8c50c7393f
Dorian Grasset <dorian.grasset.contact@gmail.com>
2025-02-12 15:52:58 +0100

    add custom scrollbar

--- src/components/shadcn/data-table.tsx
+++ src/components/shadcn/data-table.tsx
@@ -114,7 +114,13 @@ export function DataTable<TData extends { id: string | number }, TValue>({
       <DataTableToolbar table={table} />
       <div
         ref={tableRef}
-        className={cn('border-border border-1 rounded-md overflow-auto')}
+        className="border-border border-1 rounded-md overflow-auto [&::-webkit-scrollbar]:w-2
+  [&::-webkit-scrollbar-track]:rounded-full
+  [&::-webkit-scrollbar-track]:bg-gray-100
+  [&::-webkit-scrollbar-thumb]:rounded-full
+  [&::-webkit-scrollbar-thumb]:bg-gray-300
+  dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500"
       >
         <Table>
           <TableHeader>
